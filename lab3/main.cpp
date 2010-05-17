12f1d3a979129081650006968358b9dd2d77e1ab
silasb <silas.baronda@gmail.com>
2010-05-16 21:10:03 -0400

    since resize() gets called for a window creation we do not need this in initGL()

--- lab3/main.cpp
+++ lab3/main.cpp
@@ -59,20 +59,7 @@ initGL() {
 
   glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
-  glMatrixMode(GL_PROJECTION);
-  glLoadIdentity();
-  gluPerspective(60.0f*zoomFactor, 1, 1.0f, 100.0f);
-
   eye[0] = 0.0f; eye[1] = 25.0f; eye[2] = 25.0f;
   coi[0] = 0.0f; coi[1] = 25.0f; coi[2] = -25.0f;
   up[0]  = 0.0f; up[1]  = 1.0f;  up[2]  = 0.0f;
-  //eye = {0.0f, 25.0f,  25.0f};
-  //coi = {0.0f, 25.0f, -25.0f};
-  //up =  {0.0f,  1.0f,   0.0f};
-
-  glMatrixMode(GL_MODELVIEW);
-  glLoadIdentity();
-  gluLookAt(eye[0], eye[1], eye[2],
-            coi[0], coi[1], coi[2],
-             up[0],  up[1],  up[2]);
 }
